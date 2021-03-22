d44957c5eccd9af9fe0632a720ad22fe78f325af
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-22 18:07:49 -0500

    Add JUnit library and a sample test to the CI

---
+++ src/test/main/MainTest.java
@@ -0,0 +1,12 @@
+package test.main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testEmptyCollection() {
+        assertEquals(6, 3 + 3);
+    }
+}
