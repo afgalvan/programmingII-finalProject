990da932d222a95741d0cfc9cf23f93f2d15777a
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-27 11:01:07 -0500

    Suites for each test package

--- src/test/TestSuite.java
+++ src/test/TestSuite.java
@@ -2,9 +2,10 @@ package test;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import test.controllers.ControllerSuite;
 import test.main.MainTest;
-import test.models.EntityTest;
+import test.models.ModelSuite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ MainTest.class, EntityTest.class })
+@Suite.SuiteClasses({ MainTest.class, ModelSuite.class, ControllerSuite.class})
 public class TestSuite {}
---
+++ src/test/models/ModelSuite.java
@@ -0,0 +1,9 @@
+package test.models;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ EntityTest.class })
+public class ModelSuite {
+}
