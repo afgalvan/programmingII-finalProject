50295e74cd8c13b77db54dc875c63753a404d834
jwar28 <javierguerra2203@gmail.com>
2021-03-27 18:54:43 -0500

    starting user class

---
+++ src/app/models/User.java
@@ -0,0 +1,12 @@
+package app.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public abstract class User{
+    private String name;
+    private String password;
+}
