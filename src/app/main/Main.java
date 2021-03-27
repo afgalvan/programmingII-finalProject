97560a92c1302022e333e6a0f42b994f3d328edf
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-27 13:32:13 -0500

    Fix typo

--- src/app/main/Main.java
+++ src/app/main/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         PenalDocType penalDocType = new PenalDocType();
         HabeasCorpusDocType habeasCorpusDocType = new HabeasCorpusDocType();
-        System.out.println(habeasCorpusDocType.getDocTypes());
         System.out.println("De locos");
     }
 }
