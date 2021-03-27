c1c700641d0c63de4e37b234b3ff5066e264b843
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-27 18:53:39 -0500

    Add required constructor

--- src/app/models/Entity.java
+++ src/app/models/Entity.java
@@ -3,17 +3,15 @@ package app.models;
 import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @EqualsAndHashCode
+@RequiredArgsConstructor
 public abstract class Entity implements Comparable<Entity>, Serializable {
 
     private static final long serialVersionUID = 274736873469220325L;
     private final String code;
-
-    public Entity(String _code) {
-        this.code = _code;
-    }
 
     public Entity(Entity entity) {
         this(entity.getCode());
