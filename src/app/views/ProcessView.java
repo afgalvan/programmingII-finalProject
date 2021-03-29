38f9103234f8af47a00b559b71df37a14e6a24c4
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-29 01:24:25 -0500

    Format Code

--- src/app/views/ProcessView.java
+++ src/app/views/ProcessView.java
@@ -3,16 +3,15 @@ package app.views;
 import app.controllers.api.LocationApi;
 import app.controllers.document.types.HabeasCorpusDocType;
 import app.models.IdTypes;
-import app.models.ProceedingsMetadata;
-import app.models.RecordMetadata;
-import app.models.StateEntity;
 import app.models.JudicialOffice;
 import app.models.NaturalPerson;
-import app.models.Process;
-import app.models.Series;
-import app.models.SubSeries;
 import app.models.Person;
-
+import app.models.ProceedingsMetadata;
+import app.models.Process;
+import app.models.RecordMetadata;
+import app.models.Series;
+import app.models.StateEntity;
+import app.models.SubSeries;
 import java.time.LocalDate;
 import java.util.stream.Collectors;
 
