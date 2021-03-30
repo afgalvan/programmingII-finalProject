472605b5b2ead2b1b0bc90422642dbbe2b8c0299
Andres Galvan <afgalvan@unicesar.edu.co>
2021-03-30 12:48:47 -0500

    Try to push a bad code

--- src/app/main/Main.java
+++ src/app/main/Main.java
@@ -9,6 +9,7 @@ public class Main {
      * @param args Command line arguments.
      */
     public static void main(String[] args) {
+        bro("");
         ProcessView processView = new ProcessView();
         System.out.print("\033\143");
         processView.view();
