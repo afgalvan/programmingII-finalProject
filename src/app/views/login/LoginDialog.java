c8c92829c07905887d6c92e1947d62d74106fb65
Andres Galvan <afgalvan@unicesar.edu.co>
2021-04-04 18:03:26 -0500

    Don't test UI within docker

--- src/app/views/login/LoginDialog.java
+++ src/app/views/login/LoginDialog.java
@@ -1,37 +1,13 @@
 package app.views.login;
 
-import app.views.Dialog;
 import java.awt.*;
 import javax.swing.*;
 
-public class LoginDialog extends Dialog {
+public class OptionPane extends JOptionPane {
 
-    private final JFrame frame;
-    private Container container;
-    private final String message;
-
-    public LoginDialog(JFrame frame, String message, boolean modal) {
-        super(frame, modal);
-        this.message = message;
-        this.frame = frame;
-        initComponents();
+    public OptionPane(Frame frame, String response, String title, int optionPane) {
+        UIManager.put("OptionPane.background", Color.WHITE);
+        UIManager.put("OptionPane.setButtonMargin", false);
+        showConfirmDialog(frame, response, title, optionPane);
     }
-
-    public void initComponents() {
-        container = getContentPane();
-        setUndecorated(true);
-
-        setSize(300, 150);
-        setLocationRelativeTo(frame);
-        layoutConfiguration();
-
-        JLabel label = new JLabel();
-        label.setForeground(Color.BLACK);
-        label.setText(message);
-        Panel main = new Panel(new FlowLayout());
-        main.add(label);
-        container.add(main);
-    }
-
-    public void layoutConfiguration() {}
 }
