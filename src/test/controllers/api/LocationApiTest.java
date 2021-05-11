f7d7d157bf4622eac00e2e34f50bd5c2636ac5fa
Andres Galvan <afgalvan@unicesar.edu.co>
2021-05-11 02:33:21 -0500

    Ensure Test all in lowercase

--- src/test/controllers/api/LocationApiTest.java
+++ src/test/controllers/api/LocationApiTest.java
@@ -11,12 +11,12 @@ public class LocationApiTest {
     @Test
     public void getAllDepartmentsTest() {
         assertEquals(
-            "[Amazonas, Antioquia, Arauca, Atlántico, Bolívar," +
+            ("[Amazonas, Antioquia, Arauca, Atlántico, Bolívar," +
             " Boyacá, Caldas, Caquetá, Casanare, Cauca, Cesar, Chocó, Cundinamarca," +
             " Córdoba, Guainía, Guaviare, Huila, La Guajira, Magdalena, Meta, Nariño," +
             " Norte de Santander, Putumayo, Quindío, Risaralda, San Andrés y " +
             "Providencia, Santander, Sucre, Tolima, Valle del Cauca, Vaupés," +
-            " Vichada]".toLowerCase(),
+            " Vichada]").toLowerCase(),
             LocationApi.getDepartments().toString()
         );
     }
