d501bb65a1c96f04eea620197ee64ebcc5b25424
Andres Galvan <afgalvan@unicesar.edu.co>
2021-05-14 01:32:58 -0500

    Format with prettier

--- src/test/services/ServiceSuite.java
+++ src/test/services/ServiceSuite.java
@@ -4,5 +4,5 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ UserServiceTest.class, ProcessServiceTest.class})
+@Suite.SuiteClasses({ UserServiceTest.class, ProcessServiceTest.class })
 public class ServiceSuite {}
