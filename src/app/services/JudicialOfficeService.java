c1936f22c936d9be35c600dba89ffd44e578a2c7
Andres Galvan <afgalvan@unicesar.edu.co>
2021-07-01 16:58:25 -0500

    Configuration service and controllers

---
+++ src/app/services/JudicialOfficeService.java
@@ -0,0 +1,33 @@
+package app.services;
+
+import app.models.Response;
+import app.models.metadata.JudicialOffice;
+import java.util.List;
+
+public class JudicialOfficeService implements IJudicialOfficeService {
+
+    @Override
+    public Response<JudicialOffice> insert(JudicialOffice data) {
+        return null;
+    }
+
+    @Override
+    public Response<List<JudicialOffice>> getAll() {
+        return null;
+    }
+
+    @Override
+    public Response<JudicialOffice> getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Response<JudicialOffice> updateById(Integer id, JudicialOffice newData) {
+        return null;
+    }
+
+    @Override
+    public Response<JudicialOffice> deleteById(Integer id) {
+        return null;
+    }
+}
